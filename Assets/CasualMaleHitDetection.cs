1802011b98ea8d1ae4e577651f658f359427eece
tmmrtn <51034485+tmmrtn@users.noreply.github.com>
2020-04-22 09:45:22 +1000

    Added walking motion and hit detection to casual male NPC

---
+++ Assets/CasualMaleHitDetection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasualMaleHitDetection : MonoBehaviour
+{
+    Rigidbody rb;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Damaging")
+        {
+            Debug.Log("CasualMale he got hit!");
+            rb.constraints = RigidbodyConstraints.None;
+            animator.SetBool("IsDown", true);
+        }
+
+    }
+
+}
