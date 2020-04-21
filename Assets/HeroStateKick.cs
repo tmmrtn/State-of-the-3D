3557ea00720171bbb26dffe0615c86c4645596f6
tmmrtn <51034485+tmmrtn@users.noreply.github.com>
2020-04-21 12:09:33 +1000

    Added hitbox to right foot
    And also a tag on the animation frames for the foor to me 'damaging' / 'nondamaging'.

--- Assets/HeroStateKick.cs
+++ Assets/HeroStateKick.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class HeroStateKick : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    GameObject rFoot;
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rFoot = GameObject.Find("foot.R");
+        rFoot.tag = "NonDamaging";
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //
+        //      Enable the hitbox on the punching hand only between certain frames
+        //      because not all frames of the animation are supposed to be damaging (e.g., 'winding up' the punch).
+        //
+
+        if (stateInfo.normalizedTime >= 0.35f && stateInfo.normalizedTime < 0.65f)
+        {
+            rFoot.tag = "Damaging";
+        }
+        else
+            rFoot.tag = "NonDamaging";
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rFoot.tag = "NonDamaging";
+
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
